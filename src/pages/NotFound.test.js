1b046858e0a159788076e0563a6a6b597f418b12
LiangchengR <liangchengr@gmail.com>
2021-10-28 12:04:10 -0700

    added jest/enzyme testing to test if header has text, if NotFound component has img tag, and if NotFound component img tag has a src file of 404.jpeg

---
+++ src/pages/NotFound.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Enzyme, { shallow } from 'enzyme'
+import NotFound from './NotFound.js'
+import CatError from '../assets/404.jpeg'
+import Adapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe('when NotFound loads', () => {
+    it('an image is rendered', () => {
+        const notFound = shallow(<NotFound />)
+        const imgWrapper = notFound.find('img')
+        expect(imgWrapper.length).toEqual(1)    
+    })
+
+    it('the image src is 404.jpeg', () => {
+        const notFound = shallow(<NotFound />)
+        const imgWrapper = notFound.find('img')
+        console.log(imgWrapper.prop())
+        expect(imgWrapper.prop('src')).toEqual('404.jpeg')    
+    })
+
+
+})
