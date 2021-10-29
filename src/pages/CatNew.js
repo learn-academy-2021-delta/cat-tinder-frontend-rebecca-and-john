ab3919a6be263b0b81bcbae3c6b77e22029a7f11
LiangchengR <liangchengr@gmail.com>
2021-10-29 11:19:36 -0700

    added input form in CatNew and  added function to handle events and set state

--- src/pages/CatNew.js
+++ src/pages/CatNew.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react'
+import {Form, FormGroup, Label, Input} from 'reactstrap'
 
 export default class CatNew extends Component {
-    render() {
-        return (
-            <>
-                Cat New
-            </>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: {
+        name: "",
+        age: "",
+        enjoys: "",
+      },
+    };
+  }
+    
+    handleChange = (e) => {
+        let { form } = this.state
+        form[e.target.name] = e.target.value
+        this.setState({form: form})
     }
+
+  render() {
+    return (
+      <>
+        <Form>
+          <FormGroup>
+            <Label for="name">Name</Label>
+            <Input
+              type="text"
+              name="name"
+              onChange={this.handleChange}
+              value={this.state.form.name}
+            />
+            <Label for="age">Age</Label>
+            <Input
+              type="number"
+              name="age"
+              onChange={this.handleChange}
+              value={this.state.form.age}
+            />
+            <Label for="enjoys">Enjoys</Label>
+            <Input
+              type="text"
+              name="enjoys"
+              onChange={this.handleChange}
+              value={this.state.form.enjoys}
+            />
+          </FormGroup>
+            </Form>
+            {console.log(this.state.form)}
+      </>
+    );
+  }
 }
