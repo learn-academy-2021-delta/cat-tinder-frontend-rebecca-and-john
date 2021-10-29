b5c0e97cb8e57515ac4b84cdbd029aea28b9500d
JBrady <jbradysd@icloud.com>
2021-10-29 10:52:16 -0700

    started reactstrap styling

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
+import { Card, CardBody, CardTitle } from 'reactstrap'
 
 export default class Catindex extends Component {
   render() {
     return (
-<>
+      <>
         <h3>Meet Our Cat Friends</h3>
         {this.props.cats &&
           this.props.cats.map((cat) => {
             return (
               <p key={cat.id}>
-                <NavLink to={`/catshow/${cat.id}`}>{cat.name}</NavLink>
+                
+                  <Card>
+                    <CardBody>
+                      <CardTitle tag='h5'>{cat.name}</CardTitle>
+                    </CardBody>
+                    <img
+                      alt='Card image cap'
+                      src='https://picsum.photos/318/180'
+                      width='25%'
+                    />
+                    <CardBody>
+                      <NavLink to={`/catshow/${cat.id}`}>Cat Info</NavLink>
+                    </CardBody>
+                  </Card>
               </p>
             )
           })}
