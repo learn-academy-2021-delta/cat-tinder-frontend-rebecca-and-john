6025f4da81bd6333ad319ab8f94d72dd74ed99d2
JBrady <jbradysd@icloud.com>
2021-11-01 11:49:13 -0700

    fixing layout

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -8,29 +8,28 @@ export default class Catindex extends Component {
       <>
         <div className='catindex-container'>
           <h3>Meet Our Cat Friends</h3>
-          {this.props.cats &&
-            this.props.cats.map((cat) => {
-              return (
-                <Row key={cat.id}>
-                  {this.props.cats.map((cat) => (
-                    <Col md='4'>
-                      <Card>
-                        <CardBody>
-                          <CardTitle tag='h5'>{cat.name}</CardTitle>
-                        </CardBody>
-                        <CardImg
-                          alt='random'
-                          src='https://placekitten.com/350/200'
-                        />
-                        <CardBody>
-                          <NavLink to={`/catshow/${cat.id}`}>Cat Info</NavLink>
-                        </CardBody>
-                      </Card>
-                    </Col>
-                  ))}
-                </Row>
-              )
-            })}
+
+          <Row>
+            {this.props.cats &&
+              this.props.cats.map((cat) => {
+                return (
+                  <Col md='4' key={cat.id}>
+                    <Card>
+                      <CardBody>
+                        <CardTitle tag='h5'>{cat.name}</CardTitle>
+                      </CardBody>
+                      <CardImg
+                        alt='random'
+                        src='https://placekitten.com/350/200'
+                      />
+                      <CardBody>
+                        <NavLink to={`/catshow/${cat.id}`}>Cat Info</NavLink>
+                      </CardBody>
+                    </Card>
+                  </Col>
+                )
+              })}
+          </Row>
         </div>
       </>
     )
