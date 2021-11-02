512877ecd23be116d5495b07179c09bf511b9fc2
JBrady <jbradysd@icloud.com>
2021-11-01 22:15:54 -0700

    adding test styling and background image placeholder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react'
 import leftSplash from '../assets/leftSplash.png'
 import rightSplash from '../assets/rightSplash.png'
+import Background from '../assets/AdobeStock_193410438_Preview.jpeg'
 
 export default class Header extends Component {
-    render() {
-        return (
-          <>    
-              <header>
-                  <div className='header-title'>
-                    <img src={leftSplash} alt="three sideways tear drops" />
-                    <span> Friendly Felines </span>
-                    <img src={rightSplash} alt="three sideways tear drops" />
-                </div>
-              </header>
-          </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <header
+          className='header'
+          style={{ backgroundImage: 'url(' + Background + ')' }}
+        >
+          <div className='header-title'>
+            <img
+              className='teardrop'
+              src={leftSplash}
+              alt='three sideways tear drops'
+            />
+            <span> Friendly Felines </span>
+            <img
+              className='teardrop'
+              src={rightSplash}
+              alt='three sideways tear drops'
+            />
+          </div>
+        </header>
+      </>
+    )
+  }
 }
