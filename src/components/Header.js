562c8c0eb16b64f2da59f52b65422310e094a4e4
JBrady <jbradysd@icloud.com>
2021-11-02 12:40:17 -0700

    styling adjustments

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import leftSplash from '../assets/leftSplash.png'
 import rightSplash from '../assets/rightSplash.png'
-import Background from '../assets/AdobeStock_193410438_Preview.jpeg'
+// import Background from '../assets/AdobeStock_318318411.jpeg'
+import headerImage from '../assets/AdobeStock_318318411.jpeg'
+
 
 export default class Header extends Component {
   render() {
     return (
       <>
-        <header
-          className='header'
-          style={{ backgroundImage: 'url(' + Background + ')' }}
-        >
+        <header className='header'>
+          <img
+            src={headerImage}
+            className='headerImage img-fluid'
+            alt='cat wearing a bowtie'
+          />
           <div className='header-title'>
             <img
               className='teardrop'
