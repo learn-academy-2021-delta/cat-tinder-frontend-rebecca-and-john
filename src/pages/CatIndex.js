b1e048d1725907dc6f098d1ba7ce950e2fb027be
LiangchengR <liangchengr@gmail.com>
2021-11-03 12:02:48 -0700

    added some cat index styling

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -13,14 +13,16 @@ export default class Catindex extends Component {
             {this.props.cats &&
               this.props.cats.map((cat) => {
                 return (
-                  <Col md='4' className='pb-3' key={cat.id}>
-                    <Card>
+                  <Col md="4" className="pb-3" key={cat.id}>
+                    <Card
+                      className="shadow p-3 mb-5 bg-white rounded hvr-grow"
+                      >
                       <CardBody>
-                        <CardTitle tag='h5'>{cat.name}</CardTitle>
+                        <CardTitle tag="h5">{cat.name}</CardTitle>
                       </CardBody>
                       <CardImg
-                        alt='random'
-                        src='https://placekitten.com/350/200'
+                        alt="random"
+                        src="https://placekitten.com/350/200"
                       />
                       <CardBody>
                         <NavLink to={`/catshow/${cat.id}`}>
@@ -29,7 +31,7 @@ export default class Catindex extends Component {
                       </CardBody>
                     </Card>
                   </Col>
-                )
+                );
               })}
           </Row>
         </div>
@@ -37,3 +39,4 @@ export default class Catindex extends Component {
     )
   }
 }
+//
